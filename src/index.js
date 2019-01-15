364d10d5b53bdf39eb509ca9079667e14ff19ea8
Fabian Vilers <fabian.vilers@dev-one.com>
2019-01-15 14:38:06 +0100

    Implement the logger class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('hi');
+import { isBoolean, isDate, isFunction, isNumber, isString } from 'lodash';
+import chalk from 'chalk';
+
+const shouldSerialize = arg =>
+  !isBoolean(arg) &&
+  !isDate(arg) &&
+  !isFunction(arg) &&
+  !isNumber(arg) &&
+  !isString(arg);
+const toString = arg =>
+  shouldSerialize(arg) ? JSON.stringify(arg) : arg.toString();
+
+class Logger {
+  log(message, ...optionalParams) {
+    this._writeLineTo(process.stdout, chalk.reset, message, optionalParams);
+  }
+
+  warn(message, ...optionalParams) {
+    this._writeLineTo(process.stdout, chalk.yellow, message, optionalParams);
+  }
+
+  error(message, ...optionalParams) {
+    this._writeLineTo(process.stderr, chalk.red, message, optionalParams);
+  }
+
+  _writeLineTo(stream, style, message, optionalParams) {
+    const buffer = Buffer.from(
+      style([message, ...optionalParams.map(toString)].join(' '))
+    );
+
+    stream.write(buffer, optionalParams);
+    stream.write('\n');
+  }
+}
+
+export default new Logger();
